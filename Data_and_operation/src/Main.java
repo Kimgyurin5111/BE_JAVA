cd123867b01127765431705e0f6fd0a40e5085fd
kimgyurin <koong5111@naver.com>
2026-07-05 13:56:46 +0900

    20260705_JAVA_DataType

---
+++ Data_and_operation/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(6); // Number
+        System.out.println("six"); // String
+
+        System.out.println("6"); // String 6
+
+        System.out.println(6+6); // 12
+        System.out.println("6"+"6"); // 66 (결합 연산자)
+
+        System.out.println(6*6); // 36
+        //System.out.println("6"*"6"); // 문자열과 문자열은 곱셈 불가능
+
+        System.out.println("1111".length()); // 4 (문자열 길이)
+        //System.out.println(1111.length()); // 숫자는 길이 확인 불가능
+
+        System.out.println("Hello World"); //String 문자열
+        System.out.println('H'); //Char 문자
+        System.out.println("H"); //String 문자열
+    }
+}
