d1029987eec5132f1fd9e599a34b983e99fc8d55
kimgyurin <koong5111@naver.com>
2026-07-05 15:11:25 +0900

    260705_JAVA_Programming

---
+++ Programming/src/OkJavaGoInHome.java
@@ -0,0 +1,27 @@
+// 클래스 불러오기
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoInHome {
+    public static void main(String[] args) {
+
+        // 자바 아파트의 507 엘리베이터
+        String id = "JAVA APT 507";
+
+        // Elevator call
+        Elevator myElevator = new Elevator(id);
+        myElevator.callForUp(1);
+
+        // Security off
+        Security mySecurity = new Security(id);
+        mySecurity.off();
+
+        // Light on
+        Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+        hallLamp.on();
+
+        Lighting floorLamp = new Lighting(id+" / floorLamp");
+        floorLamp.on();
+    }
+}
